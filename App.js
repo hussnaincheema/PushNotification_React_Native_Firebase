ed3b09feb3a580887e3e158b1fae17c39507f011
Hussnain Cheema <cheemahussnain155@gmail.com>
2026-02-18 17:37:55 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
